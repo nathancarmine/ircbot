4df687193cac6ceda5b3ca9fc99a32ec5c9ae53a
nathancarmine <natec@res1-187-20-dhcp.int.colorado.edu>
2015-04-27 19:44:41 -0600

    Refined math function, now supports floats

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
+#include "IrcBot.cpp"
+#include <cstdlib>
 #include <iostream>
-//#include "IrcBot.cpp"
-#include "IrcBot.h"
-#include <stdlib.h>
 
 using namespace std;
 
 
 int main(int argc, const char* argv[]) {
-		//quotes are required around channel input
+	//quotes are required around channel input
 	if(argv[3] == NULL) {
 		cout<<"Quotes are required around the channel input"<<endl;
 		exit(EXIT_FAILURE);
@@ -15,7 +14,7 @@ int main(int argc, const char* argv[]) {
 	IrcBot bot = IrcBot(argv[1], argv[2], argv[3], "USER guest word1 word2: user\r\n");
 
 	bot.start();
-
+	
 
   return 0;
 }
