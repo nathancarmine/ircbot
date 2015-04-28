aea56f17d23793c1ef9f004edce0805d288ebb9f
nathancarmine <nathanacarmine@gmail.com>
2015-04-28 17:29:01 -0600

    Added new condition for main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//#include "IrcBot.h"
 #include "IrcBot.cpp"
 #include <cstdlib>
 #include <iostream>
@@ -7,15 +6,21 @@ using namespace std;
 
 
 int main(int argc, const char* argv[]) {
-		//quotes are required around channel input
+	//quotes are required around channel input
 	if(argv[3] == NULL) {
 		cout<<"Quotes are required around the channel input"<<endl;
+		exit(EXIT_FAILURE);
+	}
+	string chan = argv[3];
+	char octo = chan[0];
+	if(octo != '#') {
+		cout<<"Remember to put a # before the channel name!"<<endl;
 		exit(EXIT_FAILURE);
 	}
 	IrcBot bot = IrcBot(argv[1], argv[2], argv[3], "USER guest word1 word2: user\r\n");
 
 	bot.start();
-
+	
 
   return 0;
 }
